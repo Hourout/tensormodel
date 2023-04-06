005f1d1b25580100ba65205dce467022ab0262f5
JinQing Lee <hourout@163.com>
2023-04-06 10:28:48 +0800

    new version 0.1.1

--- tensormodel/_ocr_idcard.py
+++ tensormodel/_ocr_idcard.py
@@ -7,7 +7,6 @@ import linora as la
 __all__ = ['OCRIDCard']
 
 
-
 class OCRIDCard():
     def __init__(self):
         self.ocr = paddleocr.PaddleOCR(show_log=False)
@@ -17,15 +16,16 @@ class OCRIDCard():
         self._char_address = ['住址', '佳址', '主址', '住 址', '往址', '生址', '佳道']
         self._char_organization = ['签发机关', '鑫发机关', '金设机关', '签发物关']
         self._char_number = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+        self._keys = ['user_name', 'user_sex', 'user_nation', 'user_born', 'user_address', 'user_number', 'user_face', 'user_card']
         
-    def predict(self, image, axis=False):
+    def predict(self, image, back=True, axis=False):
         self._axis = None
         if isinstance(image, str):
             image = la.image.read_image(image)
             self._image = la.image.color_convert(image)
-        self._direction_transform(self._image)
+        self._direction_transform(self._image, back)
         if isinstance(self._info, str):
-            self._direction_transform(la.image.enhance_brightness(self._image, 0.8))
+            self._direction_transform(la.image.enhance_brightness(self._image, 0.8), back)
         if isinstance(self._info, str):
             return self._info
         self._axis_transform_up()
@@ -33,7 +33,7 @@ class OCRIDCard():
         for i in self._info:
             if '图片模糊' in self._info[i]:
                 self._temp = self._info.copy()
-                self._direction_transform(la.image.enhance_brightness(self._image, 0.8))
+                self._direction_transform(la.image.enhance_brightness(self._image, 0.8), back)
                 self._axis_transform_up()
                 self._axis_transform_down()
                 if isinstance(self._info, str):
@@ -51,7 +51,7 @@ class OCRIDCard():
                 return {'info':self._info, 'axis':self._axis, 'angle_up':self._angle_up}
         return self._info
         
-    def _direction_transform(self, image):
+    def _direction_transform(self, image, back):
         state_up = False
         state_down = False
         self._result_up = []
@@ -85,20 +85,24 @@ class OCRIDCard():
                     self._result_up = result.copy()
                     self._angle_up = angle
 
-            if not state_down:
-                rank = [0,0]
-                for r, i in enumerate(result[0], start=1):
-                    if '中华人民共和国' in i[1][0] or '居民身份证' in i[1][0]:
-                        rank[0] = r
-                    elif '机关' in i[1][0] or '有效期限' in i[1][0]:
-                        rank[1] = r
-                if rank[1]>rank[0]:
-                    state_down = True
-                    self._result_down = result.copy()
-                    self._angle_up = angle
-            
-            if state_down and state_up:
-                break
+            if back:
+                if not state_down:
+                    rank = [0,0]
+                    for r, i in enumerate(result[0], start=1):
+                        if '中华人民共和国' in i[1][0] or '居民身份证' in i[1][0]:
+                            rank[0] = r
+                        elif '机关' in i[1][0] or '有效期限' in i[1][0]:
+                            rank[1] = r
+                    if rank[1]>rank[0]:
+                        state_down = True
+                        self._result_down = result.copy()
+                        self._angle_up = angle
+                        
+                if state_down and state_up:
+                    break
+            else:
+                if state_up:
+                    break
         
         self._info = {}
         if state_up:
@@ -108,13 +112,18 @@ class OCRIDCard():
             self._info['user_born'] = '图片模糊:未识别出出生日期'
             self._info['user_address'] = '图片模糊:未识别出地址'
             self._info['user_number'] = '图片模糊:未识别出身份证号码'
-        if state_down:
-            self._info['user_type'] = '居民身份证'
-            self._info['user_organization'] = '图片模糊:未识别出签发机关'
-            self._info['user_validity_period'] = '图片模糊:未识别出有效期限'
-        if state_up == state_down == False:
-            self._info = '图片模糊:未识别出有效信息'
-            self._error = '图片模糊:未识别出有效信息'
+        if back:
+            if state_down:
+                self._info['user_type'] = '居民身份证'
+                self._info['user_organization'] = '图片模糊:未识别出签发机关'
+                self._info['user_validity_period'] = '图片模糊:未识别出有效期限'
+            if state_up == state_down == False:
+                self._info = '图片模糊:未识别出有效信息'
+                self._error = '图片模糊:未识别出有效信息'
+        else:
+            if not state_up:
+                self._info = '图片模糊:未识别出有效信息'
+                self._error = '图片模糊:未识别出有效信息'
     
     def _axis_transform_up(self):
         if len(self._result_up)==0:
@@ -315,6 +324,11 @@ class OCRIDCard():
         self._axis['user_face'][0] = self._axis['user_address'][2]+10
         self._axis['user_face'][2] = self._axis['user_face'][0]+(self._axis['user_address'][2]-self._axis['user_address'][0])*0.8
         self._axis['user_face'][3] = min(self._axis['user_face'][3], self._axis['user_number'][1]-10)
+        self._axis['user_card'][0] = self._axis['user_address'][0]-(self._axis['user_address'][2]-self._axis['user_address'][0])*0.6
+        self._axis['user_card'][2] = self._axis['user_face'][0]+(self._axis['user_address'][2]-self._axis['user_address'][0])
+        self._axis['user_card'][3] = self._axis['user_number'][1]+(self._axis['user_number'][3]-self._axis['user_number'][1])*3
+        for i in self._axis:
+            self._axis[i] = [int(max(0, j)) for j in self._axis[i]]
     
     def _axis_transform_down(self):
         if len(self._result_down)==0:
@@ -368,6 +382,52 @@ class OCRIDCard():
                                 temp = temp[temp.find(j)+1:]
                         if len(temp)==16:
                             self._info['user_validity_period'] = f'{temp[:4]}.{temp[4:6]}.{temp[6:8]}-{temp[8:12]}.{temp[12:14]}.{temp[14:16]}'
-                        
+    
+    def draw_mask(self, image=None, axis=None, box_axis='all', mask_axis=None):
+        if image is None:
+            image = self._image.copy()
+        if axis is None:
+            axis = self._axis
+
+        if box_axis=='all':
+            box_axis = self._keys
+        elif isinstance(box_axis, str):
+            if box_axis in self._keys:
+                box_axis = [box_axis]
+            else:
+                raise ValueError(f'`box_axis` must be one of {self._keys}')
+        elif isinstance(box_axis, list):
+            for i in box_axis:
+                if i not in self._keys:
+                    raise ValueError(f'`{i}` not in {self._keys}')
+        else:
+            raise ValueError(f'`box_axis` must be one of {self._keys}')
+
+
+        if mask_axis is None:
+            mask_axis = []
+        elif mask_axis=='all':
+            mask_axis = self._keys
+        elif isinstance(mask_axis, str):
+            if mask_axis in self._keys:
+                mask_axis = [mask_axis]
+            else:
+                raise ValueError(f'`box_axis` must be one of {self._keys}')
+        elif isinstance(mask_axis, list):
+            for i in mask_axis:
+                if i not in self._keys:
+                    raise ValueError(f'`{i}` not in {self._keys}')
+        else:
+            raise ValueError(f'`box_axis` must be one of {self._keys}')
+
+        t = [la.image.box_convert(axis[i], 'xyxy', 'axis') for i in box_axis if i not in mask_axis]
+        if len(t)>0:
+            image = la.image.draw_box(image, t, width=2)
+        t = [la.image.box_convert(axis[i], 'xyxy', 'axis') for i in mask_axis]
+        if len(t)>0:
+            image = la.image.draw_box(image, t, fill_color=(255,255,255))
+        return image
+
+
 
 
