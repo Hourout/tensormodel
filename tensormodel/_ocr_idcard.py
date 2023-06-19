bfc5f90afdccec605460b5762eceeccc28e0b57b
JinQing Lee <hourout@163.com>
2023-06-19 17:46:41 +0800

    new version 0.1.1

--- tensormodel/_ocr_idcard.py
+++ tensormodel/_ocr_idcard.py
@@ -632,16 +632,16 @@ class OCRIDCard():
             if len(label)==3:
                 user_organization += 1
                 user_validity_period += 1
-            elif len(label)==6:
+            elif len(label)>=6:
                 user_name += 1
                 user_sex += 1
                 user_nation += 1
                 user_born += 1
                 user_address += 1
                 user_number += 1
-            elif len(label)==9:
-                user_organization += 1
-                user_validity_period += 1
+                if len(label)==9:
+                    user_organization += 1
+                    user_validity_period += 1
         ok = name+sex+nation+born+address+number+organization+validity_period
         total = user_name+user_sex+user_nation+user_born+user_address+user_number+user_organization+user_validity_period
         result = {'name_acc':name/user_name, 'sex_acc':sex/user_sex, 'nation_acc':nation/user_nation, 
