13273407bce368ad8c8ed607d57eb0f73f282868
JinQing Lee <hourout@163.com>
2023-07-24 17:35:31 +0800

    new version 0.1.1

--- tensormodel/_ocr_hukouben.py
+++ tensormodel/_ocr_hukouben.py
@@ -17,6 +17,8 @@ class OCRHuKouBen():
         self._char_household_address = ['住址']
         self._char_register_name = ['姓名']
 #         self._char_number = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+        self._mode = ''
+        self._result = ''
         
     def predict(self, image, axis=False, ocr_result=None):
         self._axis = None
